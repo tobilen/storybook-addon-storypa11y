87a8e3347d423d6ddb8e74700ce0ec889a962a59
Tobias Lengsholz <kontakt@lhwebsolutions.de>
2018-11-30 12:09:21 +0100

    Make sure we don't run chrome in sandbox mode

--- src/api/jestMatcher.js
+++ src/api/jestMatcher.js
@@ -4,9 +4,19 @@ const { printReceived } = require("jest-matcher-utils");
 
 const pa11yTest = async (url, options = {}) => {
   const ignore = !options || !options.ignore ? [] : options.ignore;
+  const chromeLaunchConfig =
+    !options || !options.chromeLaunchConfig ? [] : options.chromeLaunchConfig;
+  const chromeLaunchConfigArgs =
+    !chromeLaunchConfig || !chromeLaunchConfig.args
+      ? []
+      : chromeLaunchConfig.args;
 
   try {
     const details = await pa11yCli(url, {
+      chromeLaunchConfig: {
+        args: ["--no-sandbox", ...chromeLaunchConfigArgs],
+        ...chromeLaunchConfig
+      },
       ignore: ["WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2", ...ignore],
       includeWarnings: true,
       ...options
@@ -18,8 +28,8 @@ const pa11yTest = async (url, options = {}) => {
 };
 
 const formatIssues = details => {
-  if(details instanceof Error) {
-    return details
+  if (details instanceof Error) {
+    return details;
   }
 
   return details.issues
@@ -39,8 +49,7 @@ export async function toBeAccessible(receivedUrl, pa11yOptions) {
 
   return {
     message: pass
-      ? () =>
-          `Expected ${receivedUrl} not to be accessible`
+      ? () => `Expected ${receivedUrl} not to be accessible`
       : () =>
           `Expected ${receivedUrl} to be accessible, but:\n` +
           `${formatIssues(details)}`,
